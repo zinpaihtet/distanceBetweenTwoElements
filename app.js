501835946cbe999c9a0da224a301348f6e02b207
zinpaihtet <zinpainghtet@qualymm.com>
2022-12-11 18:06:38 +0630

    padding added

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// distanceY('.box2','.box1')
+// distanceX('.box1','.box2')
+
+// distanceX('.box1','.box3')
+// distanceX('.box1','.box4')
+// distanceX('.box2','.box3')
+// distanceX('.box2','.box4')
+
+
+// distanceY('.box1','.box3')
+// distanceY('.box1','.box4')
+// distanceY('.box2','.box3')
+// distanceY('.box2','.box4')
+
+
+// getPadding('.box1')
--- measure.js
+++ measure.js
@@ -3,12 +3,14 @@ const selectDOM = (sel)=> document.querySelector(sel);
 const variableToString = (obj)=>{
   return obj.className || obj.id || obj.localName
 }
+
+// distance along vertical direction
 const distanceY = (One ,Two)=>{
     const eleOne = selectDOM(One);
     const eleTwo = selectDOM(Two);
     let dis=  eleTwo.getBoundingClientRect().top - eleOne.getBoundingClientRect().bottom
     if(dis >= 0){
-        console.log(`distance along Y  :  between %c${variableToString(eleOne)}  %cand %c${variableToString(eleTwo)}   %c${dis} px`,
+        console.log(`distance along Y  | :  between %c${variableToString(eleOne)}  %cand %c${variableToString(eleTwo)}   %c${dis} px`,
         'color : red',
         'color : black',
         'color:blueviolet',
@@ -17,7 +19,7 @@ const distanceY = (One ,Two)=>{
     }else{
 
      let dis= eleOne.getBoundingClientRect().top - eleTwo.getBoundingClientRect().bottom
-     console.log(`distance along Y  :  between %c${variableToString(eleTwo)}  %cand %c${variableToString(eleOne)}   %c${dis} px`,
+     console.log(`distance along Y  | :  between %c${variableToString(eleTwo)}  %cand %c${variableToString(eleOne)}   %c${dis} px`,
      'color : red',
      'color : black',
      'color:blueviolet',
@@ -25,12 +27,14 @@ const distanceY = (One ,Two)=>{
      return null;
     }
 }
+
+// distance between horizontal direction
 const distanceX = (One , Two)=>{
     const eleOne = selectDOM(One);
     const eleTwo = selectDOM(Two);
     let dis = eleTwo.getBoundingClientRect().left - eleOne.getBoundingClientRect().right;
     if(dis >= 0){
-        console.log(`distance along X  :  between %c${variableToString(eleOne)}  %cand %c${variableToString(eleTwo)}   %c${dis} px`,
+        console.log(`distance along X  ---- :  between %c${variableToString(eleOne)}  %cand %c${variableToString(eleTwo)}   %c${dis} px`,
         'color : red',
         'color : black',
         'color:blueviolet',
@@ -40,11 +44,24 @@ const distanceX = (One , Two)=>{
 
      let dis= eleOne.getBoundingClientRect().left - eleTwo.getBoundingClientRect().right;
     
-     console.log(`distance along X  :  between %c${variableToString(eleTwo)}  %cand %c${variableToString(eleOne)}   %c${dis} px`,
+     console.log(`distance along X  ---- :  between %c${variableToString(eleTwo)}  %cand %c${variableToString(eleOne)}   %c${dis} px`,
      'color : red',
      'color : black',
      'color:blueviolet',
-     'background-color : green ; color : white ; font-size : 1.2rem ; padding : 2px'        )
+     'background-color : green ; color : white ; font-size : 1.2rem ; padding : 2px' )
      return null;
     }
 }
+// padding of element
+const getPadding = (ele)=>{
+      const element = selectDOM(ele);
+      const style = element.currentStyle || window.getComputedStyle(element);
+      const {paddingTop , paddingBottom , paddingRight , paddingLeft} = style
+      console.log( `padding of %c${variableToString(element)}` , 'background-color : green ; color : white ; font-size : 1rem ; padding : 2px', {
+        paddingTop,
+        paddingBottom,
+        paddingLeft,
+        paddingRight
+          })
+}
+
